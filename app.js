ae1233d63ae7ead515bbf88b5d28084fb2ef8085
johanne.assorin <johanne.assorin@arneogroup.com>
2021-06-14 10:14:39 +0200

    node002: update - Ajout de mongoose - Création du model d'ajout d'article - Accès à la bdd - Get, Put, Post, delete

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const Thing = require('./models/Thing.js')
+
+// Connexion à mongoose
+mongoose.connect('mongodb+srv://joh:0000@cluster0.d7kfh.mongodb.net/node-formation?retryWrites=true&w=majority',
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+ })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Headers de l'api pour passer le CORS
 app.use((req, res, next) => {
@@ -14,37 +26,77 @@ app.use(express.json())
 
 // Requete POST
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body)
-    res.status(201).json({
-        msg: 'validate'
+    // on crée une nouvelle instance de Thing
+    const thing = new Thing({
+        ...req.body
     })
+    // on sauvegarde dans la bdd
+    // une promesse est renvoyée
+    thing.save()
+        .then(() => res.status(201).json({ msg: 'Saved' }))
+        .catch(error => res.status(400).json(error))
+    console.log(req.body)
+})
+
+// Get All products
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+    .then((things) => res.status(201).json(things))
+    .catch(error => res.status(400).json(error))
+})
+
+// Get one product
+// les ':' dit a express que cette partie de la route est dynamique
+// On y a accès via .params
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+    .then((oneThing) => res.status(201).json(oneThing))
+    .catch(error => res.status(400).json(error))
+    console.log('find One')
+})
+
+// Requete PUT
+app.put('/api/stuff/:id', (req, res, next) => {
+    // Put supprime toutes les datas, il est important de bien renvoyé l'id
+    Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id})
+        .then(() => res.status(201).json({ msg: 'Updated' }))
+        .catch(error => res.status(400).json(error))
+    console.log(req.body)
+})
+
+// Requete Delete
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(201).json({ msg: 'Deleted' }))
+        .catch(error => res.status(400).json(error))
+    console.log(req.body)
 })
 
 // on definit nos routes GET, POST, PATCH etc
 // .use va traiter tous types de requête
 // .get, .post etc va être une syntaxe spécifique
-app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'id01',
-            title: 'Mon 1er objet',
-            description: 'Les infos du 1er objet',
-            imageUrl: '',
-            price: 2900,
-            userId: 'qsd'
-        },
-        {
-            _id: 'id02',
-            title: 'Mon 2eme objet',
-            description: 'Les infos du 2eme objet',
-            imageUrl: '',
-            price: 5900,
-            userId: 'qsd'
-        }
-    ]
-    res.status(200).json(stuff)
-    next()
-})
+// app.get('/api/stuff', (req, res, next) => {
+//     const stuff = [
+//         {
+//             _id: 'id01',
+//             title: 'Mon 1er objet',
+//             description: 'Les infos du 1er objet',
+//             imageUrl: '',
+//             price: 2900,
+//             userId: 'qsd'
+//         },
+//         {
+//             _id: 'id02',
+//             title: 'Mon 2eme objet',
+//             description: 'Les infos du 2eme objet',
+//             imageUrl: '',
+//             price: 5900,
+//             userId: 'qsd'
+//         }
+//     ]
+//     res.status(200).json(stuff)
+//     next()
+// })
 // app.use((req, res, next) => {
 //     // on peut utiliser .end (node) ou renvoyer un json via .json
 //     res.json({ msg: 'requête bien reçu'})
